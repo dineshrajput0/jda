2043e83f6a864604ee9d38db77e9320f75d677f5
dineshrajput0 <dineshrajput.mca@gmail.com>
2019-05-27 23:06:07 +0530

    Add files via upload

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import {DemoMaterialModule} from '../material-module';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule ],
+    imports: [ RouterModule, CommonModule,DemoMaterialModule ],
     declarations: [ HeaderComponent ],
     exports: [ HeaderComponent ]
 })
